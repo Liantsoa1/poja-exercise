46d6cd7991a0b467e82d7872576b02cf9fae02fc
liantsoa <hei.liantsoa@gmail.com>
2025-06-11 21:45:49 +0300

    feat: sending asynchronous email with /hello

--- src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.my.company.endpoint.rest.controller.health;
 
-import com.my.company.service.HelloWorldService;
+import com.my.company.endpoint.event.EventProducer;
+import com.my.company.endpoint.event.model.SendEmailRequested;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final HelloWorldService service;
+    private final EventProducer<SendEmailRequested> eventProducer;
 
     @GetMapping("/hello")
-    public String helloWorld(@RequestParam String name) {
-        return service.uploadHelloWorldMessage(name);
+    @SneakyThrows
+    public String helloWorld(@RequestParam String to) {
+        var event = SendEmailRequested.builder().to(to).build();
+        eventProducer.accept(List.of(event));
+        return "... world!";
     }
 }
